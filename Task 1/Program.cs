91039125054e1851ee5011b48ffed8b7ce6c086f
ritesh969 <rit14.36.14@gmail.com>
2025-11-19 15:31:47 +0545

    Completed Task 1 (Student class and Program.cs implemented)

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Task_1
+﻿using System;
+
+namespace Workshop4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Now creating the first object...
+            Student s1 = new Student();
+            s1.name = "Ramesh Sharma";
+            s1.age = 20;
+            s1.address = "Kathmandu";
+
+            // now creating the second object
+            Student s2 = new Student();
+            s2.name = "Sita Khadka";
+            s2.age = 22;
+            s2.address = "Pokhara";
+
+          
+            Console.WriteLine("Student 1 Details:");
+            Console.WriteLine("Name: " + s1.name);
+            Console.WriteLine("Age: " + s1.age);
+            Console.WriteLine("Address: " + s1.address);
+
+            Console.WriteLine();
+
+        
+            Console.WriteLine("Student 2 Details:");
+            Console.WriteLine("Name: " + s2.name);
+            Console.WriteLine("Age: " + s2.age);
+            Console.WriteLine("Address: " + s2.address);
+
+            Console.WriteLine();
+
+            // Printing the static field....
+            Console.WriteLine("College Name: " + Student.collegeName);
         }
     }
 }
---
+++ Task 1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Workshop4
+{
+    internal class Student
+    {
+        // creating the instance field....
+        public string name;
+        public int age;
+        public string address;
+
+        // creating the static field.......
+        public static string collegeName = "IIC College";
+    }
+}
