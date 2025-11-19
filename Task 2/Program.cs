e7df23d5875660459863466cb6281c8f6580060c
ritesh969 <rit14.36.14@gmail.com>
2025-11-19 15:42:47 +0545

    Added Task 2: Calculator class with methods and return types

---
+++ Task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Now Creating the object of Calculator....
+            Calculator calc = new Calculator();
+
+            // Calling the methods...
+            calc.PrintWelcome();
+
+            int sum = calc.Add(10, 5);
+            Console.WriteLine("Addition: " + sum);
+
+            int multiply1 = calc.Multiply(10, 5);
+            Console.WriteLine("Multiplication (10 * 5): " + multiply1);
+
+            int multiply2 = calc.Multiply(10);  
+            Console.WriteLine("Multiplication with default (10 * 1): " + multiply2);
+        }
+    }
+}
