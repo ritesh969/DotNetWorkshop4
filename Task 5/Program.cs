2d616d32d2e0c1470794c6d4c65e7f62dd41c1c3
ritesh969 <rit14.36.14@gmail.com>
2025-11-19 16:02:10 +0545

    Completed Task 5: Enums and Records

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Task_5
+﻿using System;
+
+namespace Task_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter a day: ");
+            string day = Console.ReadLine().Trim().ToLower();
+
+            DayType type;
+            if (day == "friday" || day == "saturday")
+            {
+                type = DayType.Weekend;
+            }
+            else
+            {
+                type = DayType.Weekday;
+            }
+
+            Console.WriteLine("It is: " + type);
+
+            Book book1 = new Book("C# Basics", "John Doe", 500);
+
+            Book book2 = book1 with { title = "Advanced C#", price = 750 };
+
+            // Print first object
+            Console.WriteLine("\nFirst Book Details:");
+            Console.WriteLine(book1);
+            var (title, author, price) = book2;
+
+            Console.WriteLine("\nSecond Book (After Deconstruction):");
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
         }
     }
 }
