4e7187944271545e54f505825a36093c68ba47eb
ritesh969 <rit14.36.14@gmail.com>
2025-11-19 16:07:37 +0545

    Added Player class and updated Program.cs for Task 4

---
+++ Task 4/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_4
+{
+    public class Player
+    {
+        public string playerName;
+        public int level;
+        public int health;
+        public Player()
+        {
+            Console.WriteLine("Default constructor has been called");
+        }
+
+        public Player(string name, int level, int health)
+        {
+            this.playerName = name;
+            this.level = level;
+            this.health = health;
+        }
+    }
+}
--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Task_4
+﻿using System;
+
+namespace Task_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player p1 = new Player();
+            Player p2 = new Player("Ritesh Mishra", 5, 100);
+            Console.WriteLine("\nValues from default constructor object:");
+            Console.WriteLine("Player Name: " + p1.playerName);
+            Console.WriteLine("Level: " + p1.level);
+            Console.WriteLine("Health: " + p1.health);
+
+            Console.WriteLine("\nValues from parameterized constructor object:");
+            Console.WriteLine("Player Name: " + p2.playerName);
+            Console.WriteLine("Level: " + p2.level);
+            Console.WriteLine("Health: " + p2.health);
         }
     }
 }
